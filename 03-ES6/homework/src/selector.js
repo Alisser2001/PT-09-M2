2124b4c465edbf5c9f0952b001fa08ab860cbf1d
Alisser2001 <105648476+Alisser2001@users.noreply.github.com>
2022-09-15 19:56:08 -0500

    ES6 Completed

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -1,15 +1,21 @@
 var traverseDomAndCollectElements = function(matchFunc, startEl) {
   var resultSet = [];
-
   if (typeof startEl === "undefined") {
     startEl = document.body;
   }
-
+  if(matchFunc(startEl)){
+    resultSet.push(startEl);
+  }
+  
   // recorre el árbol del DOM y recolecta elementos que matchien en resultSet
   // usa matchFunc para identificar elementos que matchien
 
   // TU CÓDIGO AQUÍ
-  
+  for(let i=0; i<startEl.children.length; i++){
+    let elements = traverseDomAndCollectElements(matchFunc, startEl.children[i])
+    resultSet = [...resultSet, ...elements];
+  }
+  return resultSet;
 };
 
 // Detecta y devuelve el tipo de selector
@@ -18,7 +24,18 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 
 var selectorTypeMatcher = function(selector) {
   // tu código aquí
-  
+  if (selector[0] === '#'){
+    return 'id'
+  } else if (selector[0] === '.') {
+    return 'class'
+  } 
+  let myArr = selector.split('');
+  for(let i=0; i<myArr.length; i++) {
+    if(myArr[i]==='.'){
+      return 'tag.class'
+    }
+  }
+  return 'tag'
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -30,13 +47,41 @@ var matchFunctionMaker = function(selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
   if (selectorType === "id") { 
-   
+   matchFunction = function(el){
+    let myIdArr = el.id.split(' ');
+    for (let i=0; i<myIdArr.length; i++) {
+      myIdArr[i] = '#'+myIdArr[i];
+    }
+    if(el.id!==null) {
+        return myIdArr.includes(selector);
+    }
+    return false;
+   }
   } else if (selectorType === "class") {
-    
+    matchFunction = function(el){
+      let myClassArr = el.className.split(' ');
+      for (let i=0; i<myClassArr.length; i++){
+        myClassArr[i] = '.'+myClassArr[i];
+      }
+      if(el.className !== null) {
+        return myClassArr.includes(selector);
+      }
+   }
   } else if (selectorType === "tag.class") {
-    
+    matchFunction = function(el){
+      let mySel = selector.split('.')
+      if (el.tagName.toLowerCase() === mySel[0]){
+        let myClassArr = el.className.split(' ');
+        if(el.className !== null) {
+          return myClassArr.includes(mySel[1]);
+        }
+      }
+      return false;
+    }
   } else if (selectorType === "tag") {
-    
+    matchFunction = function(el){
+      return el.tagName && (el.tagName.toLowerCase() === selector.toLowerCase())
+    }
   }
   return matchFunction;
 };
