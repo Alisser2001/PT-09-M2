86a94c24ffe581d151b65619f40184e1f48de6f5
Alisser2001 <105648476+Alisser2001@users.noreply.github.com>
2022-09-19 18:15:27 -0500

    04-Ajax Completed

--- 04-Ajax/homework/index.js
+++ 04-Ajax/homework/index.js
@@ -11,6 +11,7 @@ $(element) => Reune todos los elementos del tipo element en el documento*/
 $('#boton').click(()=>{
     $('#lista').empty();
     $.get(`${URL}`, (friends)=>{
+        console.log(friends)
         let list = document.getElementById('lista');
         for (let i=0; i<friends.length; i++) {
             let li=document.createElement('li');
@@ -46,15 +47,32 @@ $('#search').click(()=>{
 })
 
 $('#delete').click(()=>{
-    let id= $('#input').val();
+    let id= $('#inputDelete').val();
     if(id){
         $.ajax({
             url: `${URL}/${id}`, // http://localhost:5000/amigos/id
             type: 'DELETE',
-            success: ()=>{
+            success: ()=>{ //Esta función se ejecuta si el llamado al DELETE en el url pasado es exitoso
                 $('#success').text('Tu amigo ha sido eliminado');
+                $('#lista').empty();
+                $.get(`${URL}`, (friends)=>{
+                    let list = document.getElementById('lista');
+                    for (let i=0; i<friends.length; i++) {
+                        let li=document.createElement('li');
+                        li.id = friends[i].id;
+                        li.innerHTML = friends[i].name;
+                        list.appendChild(li);
+                    }
+                })
             }
         })
+        /*$.delete(`${URL}`, (friends)=>{
+            for(let i=0; i<friends.length; i++){                         Esta manera está mal implementada, puesto que el metodo
+                if(friends[i].id === id){                                delete es muy viejo pero Ajax tiene una implementación 
+                    friends.splice(i, 1);                                para ello
+                }
+            }
+        })*/
     } else {
         $('#success').text('Ingrese un id');
     }
