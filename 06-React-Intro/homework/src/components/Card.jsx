62963b6a674edd22f1b91474dfab78965e6b1f28
Alisser2001 <105648476+Alisser2001@users.noreply.github.com>
2022-09-28 18:47:53 -0500

    React Estilos Completed

--- 06-React-Intro/homework/src/components/Card.jsx
+++ 06-React-Intro/homework/src/components/Card.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
+import styles from '../styles/Card.module.css';
 
 export default function Card(props) {
   // acá va tu código
   let imgClima = `http://openweathermap.org/img/wn/${props.img}@2x.png`;
   return (
-    <div>
-      <div>
-        <button onClick={props.onClose}>X</button>
+    <div className={styles.container}>
+      <div className={styles.contBtn}>
+        <button onClick={props.onClose} className={styles.btnClose}>X</button>
       </div>
-      <h1>{props.name}</h1>
-      <div>
+      <h1 className={styles.h1}>{props.name}</h1>
+      <div className={styles.info}>
         <div>
-          <h2>Min</h2>
+          <h2 className={styles.h2}>Min</h2>
           <p>{props.min}°</p>
         </div>
         <div>
-          <h2>Max</h2>
+          <h2 className={styles.h2}>Max</h2>
           <p>{props.max}°</p>
         </div>
         <div>
-          <img src={imgClima} alt=""></img>
+          <img src={imgClima} alt="" className={styles.img}></img>
         </div>
       </div>
     </div>
--- 06-React-Intro/homework/src/components/SearchBar.jsx
+++ 06-React-Intro/homework/src/components/SearchBar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import styles from '../styles/searchBar.module.css';
 
 export default function SearchBar(props) {
   // acá va tu código
   let search = props.onSearch;
   return (
-    <div>
-      <input type="text" value="Ciudad..."></input>
-      <button onClick={search('London')}>Agregar</button>
+    <div className={styles.container}>
+      <input type="text" value="Ciudad..." className={styles.search}></input>
+      <button onClick={()=>search('London')} className={styles.btn}>Agregar</button>
     </div>
   )
 };
